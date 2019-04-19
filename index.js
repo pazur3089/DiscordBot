d3d2e48292b7fea1b5b2ca3b89cf534ceecef5fe
Adam Irlik <airlik@fp-instruments.com>
2019-04-19 02:46:48 +0200

    Merge

--- index.js
+++ index.js
@@ -5,20 +5,16 @@ const streamOptions = { seek: 0, volume: 1 };
 const broadcast = client.createVoiceBroadcast();
 
 const linkList = [
-  "https://www.youtube.com/watch?v=MLg83QMmlGs",
-  "https://www.youtube.com/watch?v=OjINuMEuSKA",
-  "https://www.youtube.com/watch?v=z7lZPSZFQak",
-  "https://www.youtube.com/watch?v=YEt41bYQBgE",
-  "https://www.youtube.com/watch?v=H4V2jlhWJD4",
-  "https://www.youtube.com/watch?v=zgKazTrhXmI",
-  "https://www.youtube.com/watch?v=NVpjt9gHlDw",
-  "https://www.youtube.com/watch?v=umEDct4BoGc",
-  "https://www.youtube.com/watch?v=jZLHsqOXFkc"
+	"https://www.youtube.com/watch?v=MLg83QMmlGs",
+	"https://www.youtube.com/watch?v=OjINuMEuSKA",
+	"https://www.youtube.com/watch?v=z7lZPSZFQak",
+	"https://www.youtube.com/watch?v=YEt41bYQBgE",
+	"https://www.youtube.com/watch?v=H4V2jlhWJD4",
+	"https://www.youtube.com/watch?v=zgKazTrhXmI",
+	"https://www.youtube.com/watch?v=NVpjt9gHlDw",
+	"https://www.youtube.com/watch?v=umEDct4BoGc",
+	"https://www.youtube.com/watch?v=jZLHsqOXFkc"
 ];
-
-function randomLink() {
-  return linkList[parseInt(Math.random() * linkList.length)];
-}
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -30,26 +26,30 @@ client.on("message", msg => {
       .then(msg => console.log(`Deleted message from ${msg.author.username}`))
       .catch(console.error);
   }
-	if (~msg.content.indexOf('idi')) 
-	{
-			msg.member.voiceChannel.leave();
-	}
-	if (msg.content === 'our bond')
-		asd('https://www.youtube.com/watch?v=q_fI3EWYZqU');
-	
- if (msg.content === "our music") {
-	asd(randomlink());
- }
-function asd(stream1)
-{			
-	const channel = msg.member.voiceChannel;
-	channel.join()
-		.then(connection => {
-		const stream = ytdl(stream1, { filter : 'audioonly' });
-		broadcast.playStream(stream);
-		const dispatcher = connection.playBroadcast(broadcast);
-	})
-	.catch(console.error);
-}
-	 });
-client.login('TOKEN);
+  if (~msg.content.indexOf("idi")) {
+    msg.member.voiceChannel.leave();
+  }
+  if (msg.content === "our bond")
+    playMusic("https://www.youtube.com/watch?v=q_fI3EWYZqU");
+
+  if (msg.content === "our music") {
+    playMusic(randomlink());
+  }
+
+  function randomLink() {
+    return linkList[parseInt(Math.random() * linkList.length)];
+  }
+
+  function playMusic(linkMusic) {
+    const channel = msg.member.voiceChannel;
+    channel
+      .join()
+      .then(connection => {
+        const stream = ytdl(linkMusic, { filter: "audioonly" });
+        broadcast.playStream(stream);
+        const dispatcher = connection.playBroadcast(broadcast);
+      })
+      .catch(console.error);
+  }
+});
+client.login("TOKEN");
