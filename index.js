2fc102eedc1596ec46354a9c3275c15c74f5a271
pazur3089 <45018646+pazur3089@users.noreply.github.com>
2019-04-19 12:12:34 +0200

    Update index.js
    Fixed crashes

--- index.js
+++ index.js
@@ -4,34 +4,43 @@ const ytdl = require("ytdl-core");
 const streamOptions = { seek: 0, volume: 1 };
 
 const linkList = [
-	"https://www.youtube.com/watch?v=MLg83QMmlGs",
-	"https://www.youtube.com/watch?v=OjINuMEuSKA",
-	"https://www.youtube.com/watch?v=z7lZPSZFQak",
-	"https://www.youtube.com/watch?v=YEt41bYQBgE",
-	"https://www.youtube.com/watch?v=H4V2jlhWJD4",
-	"https://www.youtube.com/watch?v=zgKazTrhXmI",
-	"https://www.youtube.com/watch?v=NVpjt9gHlDw",
-	"https://www.youtube.com/watch?v=umEDct4BoGc",
-	"https://www.youtube.com/watch?v=jZLHsqOXFkc"
+  "https://www.youtube.com/watch?v=MLg83QMmlGs",
+  "https://www.youtube.com/watch?v=OjINuMEuSKA",
+  "https://www.youtube.com/watch?v=z7lZPSZFQak",
+  "https://www.youtube.com/watch?v=YEt41bYQBgE",
+  "https://www.youtube.com/watch?v=H4V2jlhWJD4",
+  "https://www.youtube.com/watch?v=zgKazTrhXmI",
+  "https://www.youtube.com/watch?v=NVpjt9gHlDw",
+  "https://www.youtube.com/watch?v=umEDct4BoGc",
+  "https://www.youtube.com/watch?v=jZLHsqOXFkc"
 ];
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
 client.on("message", msg => {
-	console.log(`${client.status}`);
+
+  console.log(`${client.status}`);
+  //console.log(msg.content.toLowerCase());
+  msg.content = msg.content.toLowerCase();
+
   if (~msg.content.indexOf("bochen")) {
     msg
       .delete()
       .then(msg => console.log(`Deleted message from ${msg.author.username}`))
       .catch(console.error);
   }
+  //leaving user's voice channel
   if (~msg.content.indexOf("idi")) {
-    msg.member.voiceChannel.leave();
+    if (msg.member.voiceChannel != null) {
+      msg.member.voiceChannel.leave();
+    }
   }
   if (msg.content === "our bond")
     playMusic("https://www.youtube.com/watch?v=q_fI3EWYZqU");
 
+  //I dont know why we made separate function for picking randomly song
   if (msg.content === "our music") {
     playMusic(randomLink());
   }
@@ -40,14 +49,18 @@ client.on("message", msg => {
     return linkList[parseInt(Math.random() * linkList.length)];
   }
 
+  //joining and brodcasting music in user's channel
   function playMusic(linkMusic) {
-	  const channel = msg.member.voiceChannel;
-    channel.join()
+    if (msg.member.voiceChannel != null) {
+      const channel = msg.member.voiceChannel;
+      channel
+        .join()
         .then(connection => {
-        const stream = ytdl(linkMusic, { filter: "audioonly" });
-        const dispatcher = connection.playStream(stream, streamOptions);
-      })
-      .catch(console.error);
+          const stream = ytdl(linkMusic, { filter: "audioonly" });
+          const dispatcher = connection.playStream(stream, streamOptions);
+        })
+        .catch(console.error);
+    }
   }
 });
 client.login("TOKEN");
